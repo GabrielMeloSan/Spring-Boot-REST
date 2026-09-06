b646729de0820a714c55386ce9177f50828f7b87
GabrielMeloSan <gabriel.melo.santos.15@gmail.com>
2026-09-06 20:16:44 -0300

    Aplicando as outras operações

--- src/main/java/com/gabriel/Spring_Boot_REST/controllers/MathController.java
+++ src/main/java/com/gabriel/Spring_Boot_REST/controllers/MathController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UnknownFormatConversionException;
+
 @RestController
 @RequestMapping("/math")
 public class MathController {
@@ -16,6 +18,51 @@ public class MathController {
     ) throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    )throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedOperationException("Please set a numeric value");
+        return convertToDouble(numberOne.replace(",", "."))-convertToDouble(numberTwo.replace(",", "."));
+    }
+
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    )throws IllegalArgumentException{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnknownFormatConversionException("Please set a numeric value");
+        return convertToDouble(numberOne.replace(",", "."))*convertToDouble(numberTwo.replace(",", "."));
+    }
+
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    )throws IllegalArgumentException{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnknownFormatConversionException("Please set a numeric value");
+        return convertToDouble(numberOne.replace(",", "."))/convertToDouble(numberTwo.replace(",", "."));
+    }
+
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    )throws IllegalArgumentException{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnknownFormatConversionException("Please set a numeric value");
+        return (convertToDouble(numberOne.replace(",", "."))+convertToDouble(numberTwo.replace(",", ".")))/2;
+    }
+
+    @RequestMapping("/sqrt/{numberOne}")
+    public Double sqrt(
+            @PathVariable("numberOne") String numberOne
+    )throws IllegalArgumentException{
+        if(!isNumeric(numberOne)) throw new UnknownFormatConversionException("Please set a numeric value");
+        return Math.sqrt((convertToDouble(numberOne.replace(",", "."))));
+
     }
 
     private Double convertToDouble(String strnumber) throws IllegalArgumentException{
