3cab0d7e8e2682c753d40302485153a42f6b0ea9
GabrielMeloSan <gabriel.melo.santos.15@gmail.com>
2026-09-06 12:52:37 -0300

    método sum funcionando.

--- src/main/java/com/gabriel/Spring_Boot_REST/controllers/MathController.java
+++ src/main/java/com/gabriel/Spring_Boot_REST/controllers/MathController.java
@@ -12,8 +12,22 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo){
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
 
-        return 1D;
+    private Double convertToDouble(String strnumber) throws IllegalArgumentException{
+        if (strnumber == null || strnumber.isEmpty()) throw new IllegalArgumentException();
+        String number =  strnumber.replace(",", ".");
+
+        return Double.parseDouble(strnumber);
+    }
+
+    private boolean isNumeric (String strnumber){
+        if (strnumber == null || strnumber.isEmpty()) return false;
+        String number =  strnumber.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
     }
 }
